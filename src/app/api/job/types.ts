774bd819a453ca0c4eebebdf29a56fb9a5e98eed
Rashmi Sharma <rashmisharma489@gmail.com>
2025-04-30 17:38:31 -0700

    Post job integration (#5)
    * integrating with updated job schema to match ui
    
    * removing print statements
    
    * Integrated profile (#4)
    
    * finalizing job-details integration
    
    * removing unused import
    
    * addressing comments
    
    * post job integration
    
    ---------
    
    Co-authored-by: Rashmi Sharma <rashmisharma@Mac.attlocal.net>
    Co-authored-by: Rashmi Sharma <rashmisharma@Rashmis-MacBook-Air-2.local>
    Co-authored-by: Subramanya Jagadeesh <31863307+subramanyaJagadeesh@users.noreply.github.com>

--- src/app/api/job/types.ts
+++ src/app/api/job/types.ts
@@ -8,9 +8,35 @@ export interface JobDetails {
     overview: string;
     description: string,
     company: string,
-    // companyDescription: string,
-    // size: string;
-    // industry: string;
+    skills: string;
+    experience: string;
+    location: string;
+    status: number;
+    postedDate: string;
+    salaryRange: string;
+    daysPostedAgo: number;
+    benefitsAndPerks: string;
+}
+
+export interface PostJobFormData {
+    title: string;
+    overview: string;
+    description: string,
+    company: string,
+    skills: string[];
+    experience: string;
+    location: string;
+    salaryFrom?: number;
+    salaryTo?: number;
+    benefitsAndPerks: string[];
+}
+
+export interface CreatedJobResponse {
+    ID: string;
+    title: string;
+    overview: string;
+    description: string,
+    company: string,
     skills: string;
     experience: string;
     location: string;
@@ -39,3 +65,13 @@ export const jobStatusLabels: Record<number, JobStatusInfo> = {
     0: { label: "Active", colorClass: "bg-purple-500/20 text-purple-400" },
     1: { label: "Closed", colorClass: "bg-red-500/20 text-red-400" },
 }; 
+
+export interface Skill {
+    id: string;
+    name: string;
+}
+  
+export interface ExperienceRequirement {
+    id: string;
+    description: string;
+}
